615f99e896e92b21ea67760829658f52da6da77e
Robin Daugherty <robin@robindaugherty.net>
2016-02-14 22:13:43 -0500

    Update production host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'bean.rndsvc.net', user: 'robindaugherty-www', roles: %w{web}
+server 'podkayne.rndsvc.net', user: 'robindaugherty-www', roles: %w{web}
 
 # role-based syntax
 # ==================

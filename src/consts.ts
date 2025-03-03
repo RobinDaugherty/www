3cdf54c92e6469133ecbf9399efbfabc77f5830c
Robin Daugherty <robin@robindaugherty.net>
2025-03-03 09:31:41 +0100

    Simplify style, add new fonts

--- src/consts.ts
+++ src/consts.ts
@@ -4,12 +4,18 @@
 export type SiteConfig = {
 	title: string;
 	email: string;
+	githubUrl: string | undefined;
+	stackoverflowUrl: string | undefined;
+	linkedinUrl: string | undefined;
 	description: string;
 };
 
 const siteConfig: SiteConfig = {
 	title: 'Robin Daugherty',
 	email: 'robin@robindaugherty.net',
+	githubUrl: 'https://github.com/RobinDaugherty',
+	stackoverflowUrl: 'https://stackoverflow.com/users/1589422/robin-daugherty',
+	linkedinUrl: 'https://www.linkedin.com/in/robindaugherty',
 	description: 'Software engineer, systems architect, engineering manager',
 };
 
